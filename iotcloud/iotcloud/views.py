ce360f601f9673bc37814b03a4767b9f179b0284
Shubham Kumar <shubhamkumar8180323@gmail.com>
2019-09-22 23:47:57 +0530

    Update views.py

--- iotcloud/iotcloud/views.py
+++ iotcloud/iotcloud/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.models import User, auth
 from django.contrib import messages
 
+##############################
+
 def home(request):
     return render(request,"index.html")
 
